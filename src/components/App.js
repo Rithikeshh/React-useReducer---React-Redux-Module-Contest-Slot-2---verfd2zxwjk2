1d5d6f457f98595b1822b639c574fdec68497ee4
Rithikeshh <135729296+Rithikeshh@users.noreply.github.com>
2023-12-17 17:31:40 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,12 @@
-import React from 'react'
+import React from "react";
 import '../styles/App.css';
-import { useReducer } from 'react';
+import { useReducer } from "react";
 
 const ACTIONS = {
   INCREMENT: "increment",
   DECREMENT: "decrement",
   SET_SUB_NUM: "setSubNum",
-  SET_ADD_NUM: "setAddNum"
+  SET_ADD_NUM: "setAddNum",
 };
 function reducer(state, action) {
   switch (action.type) {
@@ -14,28 +14,41 @@ function reducer(state, action) {
       return {
         count: Number(state.count) + Number(state.addNum),
         addNum: state.addNum,
-        subNum: state.subNum
+        subNum: state.subNum,
       };
- 
-   
+    case ACTIONS.DECREMENT:
+      return {
+        count: Number(state.count) - Number(state.subNum),
+        addNum: state.addNum,
+        subNum: state.subNum,
+      };
     case ACTIONS.SET_ADD_NUM:
       return {
         count: state.count,
         subNum: state.subNum,
-        addNum: action.payload
+        addNum: action.payload,
+      };
+    case ACTIONS.SET_SUB_NUM:
+      return {
+        count: state.count,
+        subNum: action.payload,
+        addNum: state.addNum,
       };
     default:
       return {
         count: 10,
         subNum: 1,
-        addNum: 1
+        addNum: 1,
       };
   }
 }
 
 const App = () => {
- 
-
+  const [state, dispatch] = useReducer(reducer, {
+    subNum: 1,
+    count: 10,
+    addNum: 1,
+  });
   function onIncrement() {
     dispatch({ type: ACTIONS.INCREMENT });
   }
@@ -45,25 +58,34 @@ const App = () => {
   const onAddInput = (e) => {
     dispatch({
       type: ACTIONS.SET_ADD_NUM,
-      payload: e.target.value
+      payload: e.target.value,
     });
   };
   const onSubtractInput = (e) => {
     dispatch({
       type: ACTIONS.SET_SUB_NUM,
-      payload: e.target.value
+      payload: e.target.value,
     });
   };
   return (
     <div id="main">
-      <input id='subtractInput' value={state.subNum} onChange={onSubtractInput} /><br />
-      <button id='subtractBtn' onClick={onDecrement}>Subtract</button>
-      <p id='count'>{state.count}</p>
-      <button id='addBtn' onClick={onIncrement}>Add</button><br />
-      <input id='addInput' onChange={onAddInput} value={state.addNum} />
+      <input
+        id="subtractInput"
+        value={state.subNum}
+        onChange={onSubtractInput}
+      />
+      <br />
+      <button id="subtractBtn" onClick={onDecrement}>
+        Subtract
+      </button>
+      <p id="count">{state.count}</p>
+      <button id="addBtn" onClick={onIncrement}>
+        Add
+      </button>
+      <br />
+      <input id="addInput" onChange={onAddInput} value={state.addNum} />
     </div>
-  )
-}
-
+  );
+};
 
 export default App;
